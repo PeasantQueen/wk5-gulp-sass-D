222348bb64733c408c3176d919b0cb79def9d164
peasantqueen <danielle@peasantqueen.com>
2018-02-10 15:14:47 -0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//load gulp and gulp-sass but give them convenient names
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//define a task to read your sass files and make css
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+//watch task will rebuild your css anytime you update a sass file
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+//default task will run when you type `gulp` at the command line
+gulp.task('default', ['watch']);
+
+//task to say hello
+gulp.task('hello',function() {
+  console.log('Hello you');
+});
+
